567aa8a7564b55f8fb703cf68e7f462cf62bbf60
Matt Gibson <matthew.j.s.gibson@gmail.com>
2024-05-17 10:13:34 -0500

    name container

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v /var/run/docker.sock:/var/run/docker.sock -p 4000:3000 home-dashboard
+docker run --rm --name home-dashboard -v /var/run/docker.sock:/var/run/docker.sock -p 4000:3000 home-dashboard
