9ee7ea706bc568d35607d2ff0a16e4a0478d17dc
Matt Gibson <matthew.j.s.gibson@gmail.com>
2024-05-17 10:33:46 -0500

    run as daemon

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name home-dashboard -v /var/run/docker.sock:/var/run/docker.sock -p 4000:3000 home-dashboard
+docker run -d --rm --name home-dashboard -v /var/run/docker.sock:/var/run/docker.sock -p 4000:3000 home-dashboard
